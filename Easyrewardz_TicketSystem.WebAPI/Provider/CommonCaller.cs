756bc83069bc4ddf2fed99c8538bde2602bfa26c
Shlok Barot <shlok.barot@brainvire.com>
2019-12-17 15:58:06 +0530

    Add provider and Interface

---
+++ Easyrewardz_TicketSystem.WebAPI/Provider/CommonCaller.cs
@@ -0,0 +1,19 @@
+﻿using Easyrewardz_TicketSystem.Interface;
+using Easyrewardz_TicketSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Easyrewardz_TicketSystem.WebAPI.Provider
+{
+    public class commonCaller
+    {
+        public ICommon _commonRepository;
+
+        public string sendEmail(SMTPDetails smtpDetails, string emailToAddress, string subject, string body)
+        {
+            return _commonRepository.SendEmail(smtpDetails, emailToAddress, subject, body);
+        }
+    }
+}
