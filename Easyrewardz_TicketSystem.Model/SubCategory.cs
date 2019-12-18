5bf54d9f9e65df493d9afe614af3030492d12ce4
Shlok Barot <shlok.barot@brainvire.com>
2019-12-18 19:55:34 +0530

    sub category modal

---
+++ Easyrewardz_TicketSystem.Model/SubCategory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easyrewardz_TicketSystem.Model
+{
+   public class SubCategory
+    {
+        public int SubCategoryID { get; set; }
+
+        public string SubCategoryName { get; set; }
+
+        public int CategoryID { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public int ModifiedBy { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+        public string CreatedByName { get; set; }
+    }
+}
