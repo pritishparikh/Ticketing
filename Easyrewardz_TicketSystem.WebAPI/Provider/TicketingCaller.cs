79a915ed4cc2b8beaede34cf38ac91f40949235e
Hitesh Gopal <hitesh.gopal@brainvire.com>
2019-12-19 19:00:28 +0530

    Ticketing Caller

---
+++ Easyrewardz_TicketSystem.WebAPI/Provider/TicketingCaller.cs
@@ -0,0 +1,20 @@
+﻿using Easyrewardz_TicketSystem.Interface;
+using Easyrewardz_TicketSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Easyrewardz_TicketSystem.WebAPI.Provider
+{
+    public class TicketingCaller
+    {
+        private ITicketing _ticketList;
+        public List<TicketingDetails> GetAutoSuggestTicketList(ITicketing _ticket, string TikcketTitle)
+        {
+            _ticketList = _ticket;
+            return _ticketList.GetTicketList(TikcketTitle);
+        }
+
+    }
+}
