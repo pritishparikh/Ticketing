535139c36b9909ccefc528760ae5ac86d8871ea6
Hitesh Gopal <hitesh.gopal@brainvire.com>
2019-12-19 16:45:09 +0530

    Security caller conflicts

--- Easyrewardz_TicketSystem.WebAPI/Provider/securityCaller.cs
+++ Easyrewardz_TicketSystem.WebAPI/Provider/securityCaller.cs
@@ -19,7 +19,7 @@ namespace Easyrewardz_TicketSystem.WebAPI.Provider
             _SecurityRepository = security;
             return _SecurityRepository.validateTokenGetPermission(SecretToken,ModuleID);
         }
-        public bool UpdatePassword(string cipherEmailId,string Password)
+        public bool UpdatePassword(ISecurity security,string cipherEmailId,string Password)
         {
             CommonService objSmdService = new CommonService();
             string plainEmailId = objSmdService.Decrypt(cipherEmailId);
