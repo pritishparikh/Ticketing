2a40072611ebe221d49c1a2d4881af8226e7a679
Vipin Singh <vipin.singh.bvi@gmail.com>
2020-01-10 19:26:10 +0530

    Custom Model for ticket solved

---
+++ Easyrewardz_TicketSystem.CustomModel/CustomTicketSolvedModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easyrewardz_TicketSystem.CustomModel
+{
+   public class CustomTicketSolvedModel
+    {
+        /// <summary>
+        /// TicketID
+        /// </summary>
+        public int TicketID { get; set; }
+        /// <summary>
+        /// StatusID
+        /// </summary>
+        public int StatusID { get; set; }
+        /// <summary>
+        /// BrandID
+        /// </summary>
+        public int BrandID  { get; set; }
+        /// <summary>
+        /// CategoryID
+        /// </summary>
+        public int CategoryID { get; set; }
+        /// <summary>
+        /// SubCategoryID
+        /// </summary>
+        public int SubCategoryID { get; set; }
+        /// <summary>
+        /// IssueTypeID
+        /// </summary>
+        public int IssueTypeID { get; set; }
+        /// <summary>
+        /// PriortyID
+        /// </summary>
+        public int PriortyID { get; set; }
+         /// <summary>
+         /// ChannelOfPurchaseID
+         /// </summary>
+        public int ChannelOfPurchaseID { get; set; }
+        /// <summary>
+        /// TicketActionID
+        /// </summary>
+        public int TicketActionID { get; set; }
+        /// <summary>
+        /// Modifiedby
+        /// </summary>
+        public int Modifiedby { get; set; }
+        /// <summary>
+        /// AgentID
+        /// </summary>
+        public int? AgentID { get; set; }
+    }
+}
