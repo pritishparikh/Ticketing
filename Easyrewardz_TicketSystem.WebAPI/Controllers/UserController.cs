08e0181c49226fdf9308c8551240be6a4a63e79c
amit.kamble <amit.kamble@BVIMUMLAP-65>
2020-01-22 16:45:12 +0530

    change in UserController

--- Easyrewardz_TicketSystem.WebAPI/Controllers/UserController.cs
+++ Easyrewardz_TicketSystem.WebAPI/Controllers/UserController.cs
@@ -17,7 +17,7 @@ namespace Easyrewardz_TicketSystem.WebAPI.Controllers
     /// </summary>
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(AuthenticationSchemes = SchemesNamesConst.TokenAuthenticationDefaultScheme)]
+    //[Authorize(AuthenticationSchemes = SchemesNamesConst.TokenAuthenticationDefaultScheme)]
     public class UserController : ControllerBase
     {
         #region  Variable Declaration
@@ -88,7 +88,7 @@ namespace Easyrewardz_TicketSystem.WebAPI.Controllers
         }
 
         [HttpPost]
-        [Route("GetUserList")]
+        [Route("AddUserPersonalDetail")]
         public ResponseModel AddUserPersonalDetail([FromBody] UserModel userModel )
         {         
             ResponseModel _objResponseModel = new ResponseModel();
