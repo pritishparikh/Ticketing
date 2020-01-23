73e8649b959b91f127f1fb62a2bb21061f694fbc
Pritish Parikh <pritish.parikh@brainvire.com>
2020-01-23 17:23:46 +0530

    CityMaster model

---
+++ Easyrewardz_TicketSystem.Model/CityMaster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easyrewardz_TicketSystem.Model
+{
+    public class CityMaster
+    {
+        public int CityID { get; set; }
+        public int StateID { get; set; }
+        public string CityName { get; set; }
+        public int CityCode { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
