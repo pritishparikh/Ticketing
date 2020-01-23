2a6dc48177b38b8d6c57c69df0bcc2e27096ed91
chetan.poojari <chetan.poojari@BVIMUMLAP-20>
2020-01-23 14:01:29 +0530

    commit template creation updation and deletion

--- Easyrewardz_TicketSystem.Interface/ITemplate.cs
+++ Easyrewardz_TicketSystem.Interface/ITemplate.cs
@@ -10,5 +10,18 @@ namespace Easyrewardz_TicketSystem.Interface
         List<Template> getTemplateForNote(int IssueTypeId, int TenantID);
 
         Template getTemplateContent(int TemplateId, int TenantId);
+
+        #region Settings Template
+        int InsertTemplate(int tenantId, string TemplateName, string TemplatSubject, string TemplatBody, string issueTypes, bool isTemplateActive, int createdBy);
+
+        int DeleteTemplate(int tenantID, int TemplateID);
+
+        int UpdateTemplate(int tenantId, int TemplateID, string TemplateName, int issueType, bool isTemplateActive, int ModifiedBy);
+
+        List<TemplateModel> GetTemplates(int tenantId );
+
+
+
+        #endregion
     }
 }
--- Easyrewardz_TicketSystem.WebAPI/Controllers/TemplateController.cs
+++ Easyrewardz_TicketSystem.WebAPI/Controllers/TemplateController.cs
@@ -121,6 +121,213 @@ namespace Easyrewardz_TicketSystem.WebAPI.Controllers
         }
 
 
+
+        #region Contoller for Setting
+
+        /// <summary>
+        /// Create Template
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("CreateTemplate")]
+        public ResponseModel CreateTemplate(string TemplateName, string TemplateSubject, string TemplateBody, string issueTypes, bool isTemplateActive)
+        {
+            int insertcount = 0;
+            ResponseModel _objResponseModel = new ResponseModel();
+            int StatusCode = 0;
+            string statusMessage = "";
+            try
+            {
+                ////Get token (Double encrypted) and get the tenant id 
+                string _token = Convert.ToString(Request.Headers["X-Authorized-Token"]);
+                Authenticate authenticate = new Authenticate();
+                authenticate = SecurityService.GetAuthenticateDataFromToken(_radisCacheServerAddress, SecurityService.DecryptStringAES(_token));
+
+                SettingsCaller _newTemplate = new SettingsCaller();
+
+              
+                insertcount = _newTemplate.InsertTemplate(new TemplateService(_connectioSting), authenticate.TenantId,TemplateName, TemplateSubject,
+                    TemplateBody, issueTypes, isTemplateActive,authenticate.UserMasterID);
+
+                StatusCode =
+                insertcount == 0 ?
+                     (int)EnumMaster.StatusCode.InternalServiceNotWorking : (int)EnumMaster.StatusCode.Success;
+
+                statusMessage = CommonFunction.GetEnumDescription((EnumMaster.StatusCode)StatusCode);
+
+                _objResponseModel.Status = true;
+                _objResponseModel.StatusCode = StatusCode;
+                _objResponseModel.Message = statusMessage;
+                _objResponseModel.ResponseData = insertcount;
+
+            }
+            catch (Exception ex)
+            {
+                StatusCode = (int)EnumMaster.StatusCode.InternalServerError;
+                statusMessage = CommonFunction.GetEnumDescription((EnumMaster.StatusCode)StatusCode);
+
+                _objResponseModel.Status = true;
+                _objResponseModel.StatusCode = StatusCode;
+                _objResponseModel.Message = statusMessage;
+                _objResponseModel.ResponseData = null;
+            }
+
+            return _objResponseModel;
+        }
+
+
+        /// <summary>
+        /// Update Template
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("ModifyTemplate")]
+        public ResponseModel ModifyTemplate(int TemplateID, string TemplateName, int issueType, bool isTemplateActive)
+        {
+            int updatecount = 0;
+            ResponseModel _objResponseModel = new ResponseModel();
+            int StatusCode = 0;
+            string statusMessage = "";
+            try
+            {
+                ////Get token (Double encrypted) and get the tenant id 
+                string _token = Convert.ToString(Request.Headers["X-Authorized-Token"]);
+                Authenticate authenticate = new Authenticate();
+                authenticate = SecurityService.GetAuthenticateDataFromToken(_radisCacheServerAddress, SecurityService.DecryptStringAES(_token));
+
+                SettingsCaller _newTemplate = new SettingsCaller();
+
+
+                updatecount = _newTemplate.UpdateTemplate(new TemplateService(_connectioSting), authenticate.TenantId, TemplateID,TemplateName, issueType, isTemplateActive,
+                    authenticate.UserMasterID);
+
+                StatusCode =
+                updatecount == 0 ?
+                     (int)EnumMaster.StatusCode.InternalServiceNotWorking : (int)EnumMaster.StatusCode.Success;
+
+                statusMessage = CommonFunction.GetEnumDescription((EnumMaster.StatusCode)StatusCode);
+
+                _objResponseModel.Status = true;
+                _objResponseModel.StatusCode = StatusCode;
+                _objResponseModel.Message = statusMessage;
+                _objResponseModel.ResponseData = updatecount;
+
+            }
+            catch (Exception ex)
+            {
+                StatusCode = (int)EnumMaster.StatusCode.InternalServerError;
+                statusMessage = CommonFunction.GetEnumDescription((EnumMaster.StatusCode)StatusCode);
+
+                _objResponseModel.Status = true;
+                _objResponseModel.StatusCode = StatusCode;
+                _objResponseModel.Message = statusMessage;
+                _objResponseModel.ResponseData = null;
+            }
+
+            return _objResponseModel;
+        }
+
+
+        /// <summary>
+        /// Delete Template
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("DeleteTemplate")]
+        public ResponseModel DeleteTemplate(int TemplateID)
+        {
+            int Deletecount = 0;
+            ResponseModel _objResponseModel = new ResponseModel();
+            int StatusCode = 0;
+            string statusMessage = "";
+            try
+            {
+                ////Get token (Double encrypted) and get the tenant id 
+                string _token = Convert.ToString(Request.Headers["X-Authorized-Token"]);
+                Authenticate authenticate = new Authenticate();
+                authenticate = SecurityService.GetAuthenticateDataFromToken(_radisCacheServerAddress, SecurityService.DecryptStringAES(_token));
+
+                SettingsCaller _newTemplate = new SettingsCaller();
+                Deletecount = _newTemplate.DeleteTemplate(new TemplateService(_connectioSting), authenticate.TenantId, TemplateID );
+
+                StatusCode =
+                Deletecount == 0 ?
+                     (int)EnumMaster.StatusCode.RecordNotFound : (int)EnumMaster.StatusCode.Success;
+
+                statusMessage = CommonFunction.GetEnumDescription((EnumMaster.StatusCode)StatusCode);
+
+                _objResponseModel.Status = true;
+                _objResponseModel.StatusCode = StatusCode;
+                _objResponseModel.Message = statusMessage;
+                _objResponseModel.ResponseData = Deletecount;
+
+            }
+            catch (Exception ex)
+            {
+                StatusCode = (int)EnumMaster.StatusCode.InternalServerError;
+                statusMessage = CommonFunction.GetEnumDescription((EnumMaster.StatusCode)StatusCode);
+
+                _objResponseModel.Status = true;
+                _objResponseModel.StatusCode = StatusCode;
+                _objResponseModel.Message = statusMessage;
+                _objResponseModel.ResponseData = null;
+            }
+
+            return _objResponseModel;
+        }
+
+
+        /// <summary>
+        /// View  Template
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("GetTemplate")]
+
+        public ResponseModel GetTemplate()
+        {
+
+            ResponseModel _objResponseModel = new ResponseModel();
+            List<TemplateModel> _objresponseModel = new List<TemplateModel>();
+            int StatusCode = 0;
+            string statusMessage = "";
+            try
+            {
+                ////Get token (Double encrypted) and get the tenant id 
+                string _token = Convert.ToString(Request.Headers["X-Authorized-Token"]);
+                Authenticate authenticate = new Authenticate();
+                authenticate = SecurityService.GetAuthenticateDataFromToken(_radisCacheServerAddress, SecurityService.DecryptStringAES(_token));
+
+                SettingsCaller _newTemplate = new SettingsCaller();
+                _objresponseModel = _newTemplate.GetTemplates(new TemplateService(_connectioSting), authenticate.TenantId);
+
+                StatusCode = _objresponseModel.Count == 0 ? (int)EnumMaster.StatusCode.RecordNotFound : (int)EnumMaster.StatusCode.Success;
+
+                statusMessage = CommonFunction.GetEnumDescription((EnumMaster.StatusCode)StatusCode);
+
+                _objResponseModel.Status = true;
+                _objResponseModel.StatusCode = StatusCode;
+                _objResponseModel.Message = statusMessage;
+                _objResponseModel.ResponseData = _objresponseModel;
+
+            }
+            catch (Exception ex)
+            {
+                StatusCode = (int)EnumMaster.StatusCode.InternalServerError;
+                statusMessage = CommonFunction.GetEnumDescription((EnumMaster.StatusCode)StatusCode);
+
+                _objResponseModel.Status = true;
+                _objResponseModel.StatusCode = StatusCode;
+                _objResponseModel.Message = statusMessage;
+                _objResponseModel.ResponseData = null;
+            }
+
+            return _objResponseModel;
+        }
+
+        #endregion
+
+
         #endregion
     }
 }
