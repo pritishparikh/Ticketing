2f09942b3f4ecf907215dc5044217d0070060988
chetan.poojari <chetan.poojari@BVIMUMLAP-20>
2020-01-24 14:17:09 +0530

    commit for api for fileuploadlog view

---
+++ Easyrewardz_TicketSystem.Model/FileUploadLogs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easyrewardz_TicketSystem.Model
+{
+    public class FileUploadLogs
+    {
+
+        public int FileUploadLogsID { get; set; }
+        public string FileType { get; set; }
+        public string FileName { get; set; }
+        public string Date { get; set; }
+        public string FileUploadStatus { get; set; }
+
+        public string CreatedBy { get; set; }
+        public string CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public string ModifiedDate { get; set; }
+        
+    }
+}
---
+++ Easyrewardz_TicketSystem.Services/FileUploadService.cs
@@ -0,0 +1,80 @@
+﻿using Easyrewardz_TicketSystem.Interface;
+using Easyrewardz_TicketSystem.Model;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Easyrewardz_TicketSystem.Services
+{
+    public class FileUploadService: IFileUpload
+    {
+
+
+        MySqlConnection conn = new MySqlConnection();
+
+        public FileUploadService(string _connectionString)
+        {
+            conn.ConnectionString = _connectionString;
+        }
+        public List<FileUploadLogs> GetFileUploadLogs(int tenantId)
+        {
+            List<FileUploadLogs> objFileUploadLogLst = new List<FileUploadLogs>();
+            DataSet ds = new DataSet();
+            MySqlCommand cmd = new MySqlCommand();
+            try
+            {
+
+                conn.Open();
+                cmd.Connection = conn;
+
+                MySqlCommand cmd1 = new MySqlCommand("SP_GetFileUpoadLogs", conn);
+                cmd1.CommandType = CommandType.StoredProcedure;
+                cmd1.Parameters.AddWithValue("@_tenantID", tenantId);
+                MySqlDataAdapter da = new MySqlDataAdapter();
+                da.SelectCommand = cmd1;
+                da.Fill(ds);
+
+                if (ds != null && ds.Tables != null)
+                {
+                    if (ds.Tables[0] != null && ds.Tables[0].Rows.Count > 0)
+                    {
+                        objFileUploadLogLst = ds.Tables[0].AsEnumerable().Select(r => new FileUploadLogs()
+                        {
+                            FileUploadLogsID = Convert.ToInt32(r.Field<object>("FileUploadLogsID")),
+
+                            FileType = r.Field<object>("FileType") == System.DBNull.Value ? string.Empty : Convert.ToString(r.Field<object>("FileType")),
+                            FileName = r.Field<object>("FileName") == System.DBNull.Value ? string.Empty : Convert.ToString(r.Field<object>("FileName")),
+                            Date = r.Field<object>("FileUploadDate") == System.DBNull.Value ? string.Empty : Convert.ToString(r.Field<object>("FileUploadDate")),
+                            FileUploadStatus = r.Field<object>("FileUploadStatus") == System.DBNull.Value ? string.Empty : Convert.ToString(r.Field<object>("FileUploadStatus")),
+
+                            CreatedBy = r.Field<object>("CreatedBy") == System.DBNull.Value ? string.Empty : Convert.ToString(r.Field<object>("CreatedBy")),
+                            CreatedDate = r.Field<object>("CreatedDate") == System.DBNull.Value ? string.Empty : Convert.ToString(r.Field<object>("CreatedDate")),
+                            ModifiedBy = r.Field<object>("UpdatedBy") == System.DBNull.Value ? string.Empty : Convert.ToString(r.Field<object>("UpdatedBy")),
+                            ModifiedDate = r.Field<object>("UpdatedDate") == System.DBNull.Value ? string.Empty : Convert.ToString(r.Field<object>("UpdatedDate")),
+
+
+                        }).ToList();
+                    }
+
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                string message = Convert.ToString(ex.InnerException);
+                throw ex;
+            }
+            finally
+            {
+                if (ds != null) ds.Dispose(); conn.Close();
+            }
+
+
+            return objFileUploadLogLst;
+        }
+    }
+}
