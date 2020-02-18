652e781a82ec774cbdb651da13160e38a858bacf
Brainvire <techsupport@brainvire.com>
2020-02-18 17:54:02 +0530

    Add/Edit/Delete Features

--- Easyrewardz_TicketSystem.WebAPI/Controllers/FeatureController.cs
+++ Easyrewardz_TicketSystem.WebAPI/Controllers/FeatureController.cs
@@ -80,6 +80,93 @@ namespace Easyrewardz_TicketSystem.WebAPI.Controllers
         }
         #endregion
 
+        #region Add Feature 
+        [HttpPost]
+        [Route("AddFeature")]
+        [AllowAnonymous]
+        public ResponseModel AddFeature([FromBody] FeaturesModel objFeatures)
+        {
+            ResponseModel _objResponseModel = new ResponseModel();
+            string Result =string.Empty;
+           // FeaturePlanModel feature = new FeaturePlanModel();
+            int StatusCode = 0;
+            //int Result =0;
+            string statusMessage = "";
+            try
+            {
+                string _token = Convert.ToString(Request.Headers["X-Authorized-Token"]);
+                Authenticate authenticate = new Authenticate();
+                FeaturePlanCaller fcaller = new FeaturePlanCaller();
+               // authenticate = SecurityService.GetAuthenticateDataFromToken(_radisCacheServerAddress, SecurityService.DecryptStringAES(_token));
+
+                objFeatures.UserID = 6;
+                Result = fcaller.AddFeature(new FeaturePlanService(_connectioSting),objFeatures);
+
+                StatusCode = Result !=""  ? (int)EnumMaster.StatusCode.RecordNotFound : (int)EnumMaster.StatusCode.Success;
+
+                statusMessage = CommonFunction.GetEnumDescription((EnumMaster.StatusCode)StatusCode);
+                _objResponseModel.Status = true;
+                _objResponseModel.StatusCode = StatusCode;
+                _objResponseModel.Message = Result;
+                _objResponseModel.ResponseData = Result;
+            }
+            catch (Exception ex)
+            {
+                StatusCode = (int)EnumMaster.StatusCode.InternalServerError;
+                statusMessage = CommonFunction.GetEnumDescription((EnumMaster.StatusCode)StatusCode);
+                _objResponseModel.Status = true;
+                _objResponseModel.StatusCode = StatusCode;
+                _objResponseModel.Message = statusMessage;
+                _objResponseModel.ResponseData = null;
+            }
+            return _objResponseModel;
+        }
+        #endregion
+
+        #region Delete Feature 
+        [HttpPost]
+        [Route("DeleteFeature")]
+        [AllowAnonymous]
+        public ResponseModel DeleteFeature(int FeatureID)
+        {
+            ResponseModel _objResponseModel = new ResponseModel();
+            int Result = 0;
+            // FeaturePlanModel feature = new FeaturePlanModel();
+            int StatusCode = 0;
+            //int Result =0;
+            string statusMessage = "";
+            try
+            {
+                string _token = Convert.ToString(Request.Headers["X-Authorized-Token"]);
+                Authenticate authenticate = new Authenticate();
+                FeaturePlanCaller fcaller = new FeaturePlanCaller();
+                // authenticate = SecurityService.GetAuthenticateDataFromToken(_radisCacheServerAddress, SecurityService.DecryptStringAES(_token));
+
+                int UserID = 6;
+               
+                Result = fcaller.DeleteFeature(new FeaturePlanService(_connectioSting), UserID, FeatureID);
+
+                StatusCode = Result == 0 ? (int)EnumMaster.StatusCode.RecordNotFound : (int)EnumMaster.StatusCode.Success;
+
+                statusMessage = CommonFunction.GetEnumDescription((EnumMaster.StatusCode)StatusCode);
+                _objResponseModel.Status = true;
+                _objResponseModel.StatusCode = StatusCode;
+                _objResponseModel.Message = statusMessage;
+                _objResponseModel.ResponseData = Result;
+            }
+            catch (Exception ex)
+            {
+                StatusCode = (int)EnumMaster.StatusCode.InternalServerError;
+                statusMessage = CommonFunction.GetEnumDescription((EnumMaster.StatusCode)StatusCode);
+                _objResponseModel.Status = true;
+                _objResponseModel.StatusCode = StatusCode;
+                _objResponseModel.Message = statusMessage;
+                _objResponseModel.ResponseData = null;
+            }
+            return _objResponseModel;
+        }
+        #endregion
+
         #endregion
 
     }
