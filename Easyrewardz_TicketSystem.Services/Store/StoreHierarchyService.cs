0f3b27aed870669a193272e448edbafbaf588384
Vipin Singh <vipin.singh.bvi@gmail.com>
2020-04-02 14:21:16 +0530

    Hirerchy CRUD

---
+++ Easyrewardz_TicketSystem.Services/Store/StoreHierarchyService.cs
@@ -0,0 +1,179 @@
+﻿using Easyrewardz_TicketSystem.CustomModel;
+using Easyrewardz_TicketSystem.Interface;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Easyrewardz_TicketSystem.Services
+{
+    public class StoreHierarchyService : IStoreHierarchy
+    {
+        #region variable
+        public static string Xpath = "//NewDataSet//Table1";
+        #endregion
+
+        #region Constructor
+        MySqlConnection conn = new MySqlConnection();
+        public StoreHierarchyService(string _connectionString)
+        {
+            conn.ConnectionString = _connectionString;
+        }
+        #endregion
+        public List<string> BulkUploadStoreHierarchy(int TenantID, int CreatedBy, DataSet DataSetCSV)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CreateStoreHierarchy(CustomHierarchymodel customHierarchymodel)
+        {
+            int Success = 0;
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SP_CreateStoreHierarchy", conn)
+                {
+                    Connection = conn,
+                    CommandType = CommandType.StoredProcedure
+                };
+                cmd.Parameters.AddWithValue("@Designation_Name", customHierarchymodel.DesignationName);
+                cmd.Parameters.AddWithValue("@Reportto_ID", customHierarchymodel.ReportToDesignation);
+                cmd.Parameters.AddWithValue("@Is_Active", customHierarchymodel.IsActive);
+                cmd.Parameters.AddWithValue("@Tenant_ID", customHierarchymodel.TenantID);
+                cmd.Parameters.AddWithValue("@User_ID", customHierarchymodel.CreatedBy);
+                Success = Convert.ToInt32(cmd.ExecuteScalar());
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+            return Success;
+        }
+
+        public int DeleteStoreHierarchy(int designationID, int userMasterID, int tenantID)
+        {
+            int success = 0;
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SP_DeleteStoreHierarchy", conn)
+                {
+                    Connection = conn
+                };
+                cmd.Parameters.AddWithValue("@Hierarchy_ID", designationID);
+                cmd.Parameters.AddWithValue("@Created_By", userMasterID);
+                cmd.Parameters.AddWithValue("@Tenant_ID", tenantID);
+                cmd.CommandType = CommandType.StoredProcedure;
+                success = Convert.ToInt32(cmd.ExecuteNonQuery());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+
+            return success;
+        }
+
+        public List<CustomHierarchymodel> ListStoreHierarchy(int TenantID)
+        {
+            DataSet ds = new DataSet();
+            List<CustomHierarchymodel> listHierarchy = new List<CustomHierarchymodel>();
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SP_ListStoreHierarchy", conn)
+                {
+                    Connection = conn
+                };
+                cmd.Parameters.AddWithValue("@Tenant_ID", TenantID);
+                cmd.CommandType = CommandType.StoredProcedure;
+                MySqlDataAdapter da = new MySqlDataAdapter
+                {
+                    SelectCommand = cmd
+                };
+                da.Fill(ds);
+                if (ds != null && ds.Tables[0] != null)
+                {
+                    for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                    {
+                        CustomHierarchymodel hierarchymodel = new CustomHierarchymodel();
+                        hierarchymodel.DesignationID = Convert.ToInt32(ds.Tables[0].Rows[i]["DesignationID"]);
+                        hierarchymodel.DesignationName = Convert.ToString(ds.Tables[0].Rows[i]["DesignationName"]);
+                        hierarchymodel.ReportTo = Convert.ToString(ds.Tables[0].Rows[i]["ReportTo"]);
+                        hierarchymodel.Createdbyperson = Convert.ToString(ds.Tables[0].Rows[i]["Createdby"]);
+                        hierarchymodel.Updatedbyperson = Convert.ToString(ds.Tables[0].Rows[i]["UpdatedBy"]);
+                        hierarchymodel.Status = Convert.ToString(ds.Tables[0].Rows[i]["IsActive"]);
+                        hierarchymodel.Createddate = Convert.ToDateTime(ds.Tables[0].Rows[i]["CreatedDate"]);
+                        hierarchymodel.Createdateformat = hierarchymodel.Createddate.ToString("dd MMMM yyyy");
+                        hierarchymodel.Updateddate = Convert.ToDateTime(ds.Tables[0].Rows[i]["ModifiedDate"]);
+                        hierarchymodel.Updateddateformat = hierarchymodel.Updateddate.ToString("dd MMMM yyyy");
+                        hierarchymodel.ReportToDesignation = Convert.ToInt16(ds.Tables[0].Rows[i]["ReportToDesignation"]);
+                        listHierarchy.Add(hierarchymodel);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+            return listHierarchy;
+        }
+
+        public int UpdateStoreHierarchy(CustomHierarchymodel customHierarchymodel)
+        {
+            int Success = 0;
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand
+                {
+                    Connection = conn,
+                    CommandType = CommandType.StoredProcedure,
+                    CommandText = "SP_UpdateStoreHierarchy"
+                };
+                cmd.Parameters.AddWithValue("@Designation_ID", customHierarchymodel.DesignationID);
+                cmd.Parameters.AddWithValue("@Designation_Name", string.IsNullOrEmpty(customHierarchymodel.DesignationName) ? "" : customHierarchymodel.DesignationName);
+                cmd.Parameters.AddWithValue("@Reportto_ID", string.IsNullOrEmpty(Convert.ToString(customHierarchymodel.ReportToDesignation)) ? 0 : customHierarchymodel.ReportToDesignation);
+                cmd.Parameters.AddWithValue("@Is_Active", customHierarchymodel.IsActive);
+                cmd.Parameters.AddWithValue("@Tenant_ID", customHierarchymodel.TenantID);
+                cmd.Parameters.AddWithValue("@User_ID", customHierarchymodel.CreatedBy);
+                Success = Convert.ToInt32(cmd.ExecuteNonQuery());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+            return Success;
+        }
+    }       
+}
+
---
+++ Easyrewardz_TicketSystem.WebAPI/Provider/Store/StoreHierarchyCaller.cs
@@ -0,0 +1,69 @@
+﻿using Easyrewardz_TicketSystem.CustomModel;
+using Easyrewardz_TicketSystem.Interface;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Easyrewardz_TicketSystem.WebAPI.Provider
+{
+    public class StoreHierarchyCaller
+    {
+        #region Variable
+        public IStoreHierarchy _HierarchyRepository;
+        #endregion
+        /// <summary>
+        /// Create Store Hierarchy
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
+        public int CreateStoreHierarchy(IStoreHierarchy Hierarchy, CustomHierarchymodel customHierarchymodel)
+        {
+            _HierarchyRepository = Hierarchy;
+            return _HierarchyRepository.CreateStoreHierarchy(customHierarchymodel);
+        }
+        /// <summary>
+        /// Update Store Hierarchy
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
+        public int UpdateStoreHierarchy(IStoreHierarchy Hierarchy, CustomHierarchymodel customHierarchymodel)
+        {
+            _HierarchyRepository = Hierarchy;
+            return _HierarchyRepository.UpdateStoreHierarchy(customHierarchymodel);
+        }
+        /// <summary>
+        /// Delete Store Hierarchy
+        /// </summary>
+        /// <param name=""></param>
+        /// <param name=""></param>
+        /// <returns></returns>
+        public int DeleteStoreHierarchy(IStoreHierarchy Hierarchy, int designationID, int usermasterID,int tenantID)
+        {
+            _HierarchyRepository = Hierarchy;
+            return _HierarchyRepository.DeleteStoreHierarchy(designationID, usermasterID, tenantID);
+        }
+        /// <summary>
+        /// List Store Hierarchy
+        /// </summary>
+        /// <param name="TenantID"></param>
+        /// <param name=""></param>
+        /// <returns></returns>
+        public List<CustomHierarchymodel> ListStoreHierarchy(IStoreHierarchy Hierarchy, int TenantID)
+        {
+            _HierarchyRepository = Hierarchy;
+            return _HierarchyRepository.ListStoreHierarchy(TenantID);
+        }
+        /// <summary>
+        /// Store Hierarchy BulkUpload
+        /// </summary>
+        /// <param name="TenantID"></param>
+        /// <param name=""></param>
+        /// <returns></returns>
+        //public List<string> StoreHierarchyBulkUpload(IStoreHierarchy Hierarchy, int TenantID, int CreatedBy, DataSet DataSetCSV)
+        //{
+        //    _HierarchyRepository = Hierarchy;
+        //    return _HierarchyRepository.BulkUploadStoreHierarchy(TenantID, CreatedBy, DataSetCSV);
+        //}
+    }
+}
