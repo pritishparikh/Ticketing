9cf410482a472a95baf0a3112546190371865aff
devesh.padmashali <devesh.padmashali@BVIAHMLAP-10>
2020-04-02 17:17:00 +0530

    Error in class creation

---
+++ Easyrewardz_TicketSystem.Services/StoreService/ItemService.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Xml;
+using Easyrewardz_TicketSystem.Interface;
+using MySql.Data.MySqlClient;
+
+namespace Easyrewardz_TicketSystem.Services
+{
+    public class ItemService : IItem
+    {
+
+        #region variable
+        public static string Xpath = "//NewDataSet//Table1";
+        #endregion
+
+        /// <summary>
+        /// Get Brand list for drop down 
+        /// </summary>
+        /// <param name="EncptToken"></param>
+        /// <returns></returns>
+
+
+        MySqlConnection conn = new MySqlConnection();
+
+        public ItemService(string _connectionString)
+        {
+            conn.ConnectionString = _connectionString;
+        }
+
+        /// <summary>
+        /// Bulk Upload Category
+        /// </summary>
+        /// <param name="TenantID"></param>
+        /// <param name="CreatedBy"></param>
+        /// <param name="CategoryFor"></param>
+        /// <param name="DataSetCSV"></param>
+        /// <returns></returns>
+        public List<string> ItemBulkUpload(int TenantID, int CreatedBy, int CategoryFor, DataSet DataSetCSV)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            DataSet Bulkds = new DataSet();
+            List<string> csvLst = new List<string>();
+            string SuccessFile = string.Empty; string ErrorFile = string.Empty;
+            try
+            {
+                if (DataSetCSV != null && DataSetCSV.Tables.Count > 0)
+                {
+                    if (DataSetCSV.Tables[0] != null && DataSetCSV.Tables[0].Rows.Count > 0)
+                    {
+
+                        xmlDoc.LoadXml(DataSetCSV.GetXml());
+                        conn.Open();
+                        MySqlCommand cmd = new MySqlCommand("SP_CategoryBulkUpload", conn);
+
+                        cmd.Parameters.AddWithValue("@_xml_content", xmlDoc.InnerXml);
+                        cmd.Parameters.AddWithValue("@_node", Xpath);
+                        cmd.Parameters.AddWithValue("@Created_By", CreatedBy);
+
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        MySqlDataAdapter da = new MySqlDataAdapter
+                        {
+                            SelectCommand = cmd
+                        };
+                        da.Fill(Bulkds);
+
+                        if (Bulkds != null && Bulkds.Tables[0] != null && Bulkds.Tables[1] != null)
+                        {
+
+                            //for success file
+                            SuccessFile = Bulkds.Tables[1].Rows.Count > 0 ? CommonService.DataTableToCsv(Bulkds.Tables[1]) : string.Empty;
+                            csvLst.Add(SuccessFile);
+
+                            //for error file
+                            ErrorFile = Bulkds.Tables[0].Rows.Count > 0 ? CommonService.DataTableToCsv(Bulkds.Tables[0]) : string.Empty;
+                            csvLst.Add(ErrorFile);
+
+
+                        }
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if (DataSetCSV != null)
+                {
+                    DataSetCSV.Dispose();
+                }
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+            return csvLst;
+        }
+
+    }
+}
