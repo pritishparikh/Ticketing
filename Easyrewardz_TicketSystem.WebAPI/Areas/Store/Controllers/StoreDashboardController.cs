b127dbfb8b357f541f45bf7cbdea2bb12f1167ca
Suraj Kumthekar <suraj.kumthekar@brainvire.com>
2020-04-17 18:48:37 +0530

    new store dashboard changes

---
+++ Easyrewardz_TicketSystem.WebAPI/Areas/Store/Controllers/StoreDashboardController.cs
@@ -0,0 +1,82 @@
+﻿using Easyrewardz_TicketSystem.CustomModel;
+using Easyrewardz_TicketSystem.Model;
+using Easyrewardz_TicketSystem.Services;
+using Easyrewardz_TicketSystem.WebAPI.Filters;
+using Easyrewardz_TicketSystem.WebAPI.Provider;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace Easyrewardz_TicketSystem.WebAPI.Areas.Store.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = SchemesNamesConst.TokenAuthenticationDefaultScheme)]
+    public class StoreDashboardController : ControllerBase
+    {
+        #region variable declaration
+        private IConfiguration configuration;
+        private readonly string _connectioSting;
+        private readonly string _radisCacheServerAddress;
+        private readonly string rootPath;
+        private readonly string _UploadedBulkFile;
+        #endregion
+
+        #region Constructor
+        public StoreDashboardController(IConfiguration _iConfig)
+        {
+            configuration = _iConfig;
+            _connectioSting = configuration.GetValue<string>("ConnectionStrings:DataAccessMySqlProvider");
+            _radisCacheServerAddress = configuration.GetValue<string>("radishCache");
+            _UploadedBulkFile = configuration.GetValue<string>("FileUploadLocation");
+            rootPath = configuration.GetValue<string>("APIURL");
+        }
+        #endregion
+
+        /// <summary>
+        /// Get Stroe Dashboard Data
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("getstoreDashboardList")]
+        public ResponseModel getstoreDashboardList([FromBody] StoreDashboardModel dasbhboardmodel)
+        {
+
+            List<StoreDashboardResponseModel> objDepartmentList = new List<StoreDashboardResponseModel>();
+            ResponseModel objResponseModel = new ResponseModel();
+            int StatusCode = 0;
+            string statusMessage = "";
+            try
+            {
+                string _token = Convert.ToString(Request.Headers["X-Authorized-Token"]);
+                Authenticate authenticate = new Authenticate();
+                //authenticate = SecurityService.GetAuthenticateDataFromToken(_radisCacheServerAddress, SecurityService.DecryptStringAES(_token));
+
+                StoreDashboard newMasterBrand = new StoreDashboard();
+
+                objDepartmentList = newMasterBrand.getStoreDashboardTaskList(new StoreDashboardService(_connectioSting), dasbhboardmodel);
+
+                StatusCode =
+                objDepartmentList.Count == 0 ?
+                     (int)EnumMaster.StatusCode.RecordNotFound : (int)EnumMaster.StatusCode.Success;
+
+                statusMessage = CommonFunction.GetEnumDescription((EnumMaster.StatusCode)StatusCode);
+
+                objResponseModel.Status = true;
+                objResponseModel.StatusCode = StatusCode;
+                objResponseModel.Message = statusMessage;
+                objResponseModel.ResponseData = objDepartmentList;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return objResponseModel;
+        }
+
+    }
+}
