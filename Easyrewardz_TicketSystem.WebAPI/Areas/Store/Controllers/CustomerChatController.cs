7bb74c0952b18e476d5f670beabcf65465ed662c
Vipin Singh <vipin.singh.bvi@gmail.com>
2020-04-29 19:26:24 +0530

    Customer Chat Controller,service

---
+++ Easyrewardz_TicketSystem.WebAPI/Areas/Store/Controllers/CustomerChatController.cs
@@ -0,0 +1,82 @@
+﻿using Easyrewardz_TicketSystem.CustomModel;
+using Easyrewardz_TicketSystem.Model;
+using Easyrewardz_TicketSystem.Services;
+using Easyrewardz_TicketSystem.WebAPI.Filters;
+using Easyrewardz_TicketSystem.WebAPI.Provider;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Easyrewardz_TicketSystem.WebAPI.Areas.Store.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = SchemesNamesConst.TokenAuthenticationDefaultScheme)]
+    public class CustomerChatController : ControllerBase
+    {
+        #region variable declaration
+        private IConfiguration configuration;
+        private readonly string _connectionString;
+        private readonly string _radisCacheServerAddress;
+        #endregion
+
+        #region Constructor
+        public CustomerChatController(IConfiguration  iConfig)
+        {
+            configuration = iConfig;
+            _connectionString = configuration.GetValue<string>("ConnectionStrings:DataAccessMySqlProvider");
+            _radisCacheServerAddress = configuration.GetValue<string>("radishCache");
+        }
+        #endregion
+
+        #region Custom Methods
+        /// <summary>
+        /// Get Ongoing Chat
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("GetOngoingChat")]
+        public ResponseModel GetOngoingChat()
+        {
+            List<CustomerChatMaster> customerChatMaster = new List<CustomerChatMaster>();
+            ResponseModel objResponseModel = new ResponseModel();
+            int statusCode = 0;
+            string statusMessage = "";
+            try
+            {
+                string token = Convert.ToString(Request.Headers["X-Authorized-Token"]);
+                Authenticate authenticate = new Authenticate();
+                authenticate = SecurityService.GetAuthenticateDataFromToken(_radisCacheServerAddress, SecurityService.DecryptStringAES(token));
+
+                CustomerChatCaller customerChatCaller = new CustomerChatCaller();
+
+                customerChatMaster = customerChatCaller.OngoingChat(new CustomerChatService(_connectionString), authenticate.UserMasterID, authenticate.TenantId);
+
+                statusCode =
+               customerChatMaster.Count == 0 ?
+                    (int)EnumMaster.StatusCode.RecordNotFound : (int)EnumMaster.StatusCode.Success;
+
+                statusMessage = CommonFunction.GetEnumDescription((EnumMaster.StatusCode)statusCode);
+
+
+                objResponseModel.Status = true;
+                objResponseModel.StatusCode = statusCode;
+                objResponseModel.Message = statusMessage;
+                objResponseModel.ResponseData = customerChatMaster;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return objResponseModel;
+        }
+
+        #endregion
+    }
+}
---
+++ Easyrewardz_TicketSystem.WebAPI/Provider/Store/CustomerChatCaller.cs
@@ -0,0 +1,27 @@
+﻿using Easyrewardz_TicketSystem.Interface;
+using Easyrewardz_TicketSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Easyrewardz_TicketSystem.WebAPI.Provider
+{
+    public class CustomerChatCaller
+    {
+        #region Variable declaration
+
+        private ICustomerChat _customerChat;
+        #endregion
+
+        #region Methods 
+        public List<CustomerChatMaster> OngoingChat(ICustomerChat customerChat,int userMasterID,int tenantID)
+        {
+            _customerChat = customerChat;
+
+            return _customerChat.OngoingChat(userMasterID, tenantID);
+        }
+
+        #endregion
+    }
+}
