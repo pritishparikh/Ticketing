9754ac1ca8c40299ef32d7207627e448afdac48e
hitesh.gopal@brainvire.com <hitesh.gopal@brainvire.com>
2020-04-30 21:17:23 +0530

    Appointment Tab Details

---
+++ Easyrewardz_TicketSystem.Interface/StoreInterface/IAppointment.cs
@@ -0,0 +1,12 @@
+﻿using Easyrewardz_TicketSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easyrewardz_TicketSystem.Interface
+{
+    public interface IAppointment
+    {
+        List<AppointmentModel> GetAppointmentList(int TenantID);
+    }
+}
---
+++ Easyrewardz_TicketSystem.WebAPI/Provider/Store/AppointmentCaller.cs
@@ -0,0 +1,26 @@
+﻿using Easyrewardz_TicketSystem.Interface;
+using Easyrewardz_TicketSystem.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Easyrewardz_TicketSystem.WebAPI.Provider
+{
+    public class AppointmentCaller
+    {
+
+        #region Variable
+        public IAppointment _AppointmentRepository;
+        #endregion
+
+        #region Customer wrapper method 
+        public List<AppointmentModel> GetAppointmentList(IAppointment appointment,int tenantID)
+        {
+            _AppointmentRepository = appointment;
+            return _AppointmentRepository.GetAppointmentList(tenantID);
+        }
+        #endregion
+
+    }  
+}
