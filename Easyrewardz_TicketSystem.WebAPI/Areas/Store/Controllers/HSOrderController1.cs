a00de26b632b0f958b2e520595657e4315c0fda0
devesh.padmashali <devesh.padmashali@BVIAHMLAP-10>
2020-06-01 16:08:44 +0530

    Added multiple controller and class

---
+++ Easyrewardz_TicketSystem.WebAPI/Areas/Store/Controllers/HSOrderController1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Easyrewardz_TicketSystem.WebAPI.Filters;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Easyrewardz_TicketSystem.WebAPI.Provider;
+using Easyrewardz_TicketSystem.CustomModel;
+using Easyrewardz_TicketSystem.Model;
+using Easyrewardz_TicketSystem.Services;
+
+namespace Easyrewardz_TicketSystem.WebAPI.Areas.Store.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = SchemesNamesConst.TokenAuthenticationDefaultScheme)]
+    public partial class HSOrderController : ControllerBase
+    {
+        #region variable declaration
+        private IConfiguration configuration;
+        private readonly string _connectionString;
+        private readonly string _radisCacheServerAddress;
+        private readonly string _ClientAPIUrl;
+        #endregion
+
+        #region Constructor
+        public HSOrderController(IConfiguration iConfig)
+        {
+            configuration = iConfig;
+            _connectionString = configuration.GetValue<string>("ConnectionStrings:DataAccessMySqlProvider");
+            _radisCacheServerAddress = configuration.GetValue<string>("radishCache");
+            _ClientAPIUrl = configuration.GetValue<string>("ClientAPIURL");
+        }
+        #endregion
+
+
+    }
+}
---
+++ Easyrewardz_TicketSystem.WebAPI/Areas/Store/Controllers/HSOrderController3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Easyrewardz_TicketSystem.WebAPI.Areas.Store.Controllers
+{
+    public partial class HSOrderController : ControllerBase
+    {
+    }
+}
