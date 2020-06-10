426bc5e3a1ff41719cc91f756306e8b523ef8322
devesh.padmashali <devesh.padmashali@BVIAHMLAP-10>
2020-06-10 15:30:28 +0530

    Added new Parameter

--- Easyrewardz_TicketSystem.Model/StoreModal/HSOrderModel2.cs
+++ Easyrewardz_TicketSystem.Model/StoreModal/HSOrderModel2.cs
@@ -103,6 +103,10 @@ namespace Easyrewardz_TicketSystem.Model
         /// </summary>
         public string PickupTime { get; set; }
         /// <summary>
+        /// CountSendPaymentLink
+        /// </summary>
+        public int CountSendPaymentLink { get; set; }
+        /// <summary>
         /// OrdersItemList
         /// </summary>
         public List<OrdersItem> OrdersItemList { get; set; }
--- Easyrewardz_TicketSystem.Services/StoreService/HSOrderService2.cs
+++ Easyrewardz_TicketSystem.Services/StoreService/HSOrderService2.cs
@@ -68,6 +68,7 @@ namespace Easyrewardz_TicketSystem.Services
                             ModeOfPayment = ds.Tables[0].Rows[i]["ModeOfPayment"] == DBNull.Value ? string.Empty : Convert.ToString(ds.Tables[0].Rows[i]["ModeOfPayment"]),
                             PaymentVia = ds.Tables[0].Rows[i]["PaymentVia"] == DBNull.Value ? string.Empty : Convert.ToString(ds.Tables[0].Rows[i]["PaymentVia"]),
                             TotalAmount = ds.Tables[0].Rows[i]["TotalAmount"] == DBNull.Value ? string.Empty : Convert.ToString(ds.Tables[0].Rows[i]["TotalAmount"]),
+                            CountSendPaymentLink = ds.Tables[0].Rows[i]["CountSendPaymentLink"] == DBNull.Value ? 0 : Convert.ToInt32(ds.Tables[0].Rows[i]["CountSendPaymentLink"]),
                             OrdersItemList = new List<OrdersItem>(),
                             ShoppingBagItemList = new List<ShoppingBagItem>()
                         };
