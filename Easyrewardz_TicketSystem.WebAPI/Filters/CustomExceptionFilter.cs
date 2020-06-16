f4544eb4454277b3604c4b181e6222b14ae153e2
chetan.poojari <chetan.poojari@BVIMUMLAP-20>
2020-06-16 23:01:31 +0530

    commit : exception error log  changes implemated client elastic api

--- Easyrewardz_TicketSystem.WebAPI/Filters/CustomExceptionFilter.cs
+++ Easyrewardz_TicketSystem.WebAPI/Filters/CustomExceptionFilter.cs
@@ -14,6 +14,8 @@ using Easyrewardz_TicketSystem.WebAPI.Provider;
 using Easyrewardz_TicketSystem.Interface;
 using Easyrewardz_TicketSystem.CustomModel;
 using Microsoft.AspNetCore.Routing;
+using Easyrewardz_TicketSystem.Model.StoreModal;
+using Newtonsoft.Json;
 
 namespace Easyrewardz_TicketSystem.WebAPI.Filters
 {
@@ -27,6 +29,7 @@ namespace Easyrewardz_TicketSystem.WebAPI.Filters
         private readonly string _connectioSting;
         private readonly string _ErconnectioSting;
         private readonly string _radisCacheServerAddress;
+        private readonly string _ClientAPIUrl;
         #endregion
         public CustomExceptionFilter(IConfiguration _iConfig)
         {
@@ -34,17 +37,22 @@ namespace Easyrewardz_TicketSystem.WebAPI.Filters
             _connectioSting = configuration.GetValue<string>("ConnectionStrings:DataAccessMySqlProvider");
             _ErconnectioSting = configuration.GetValue<string>("ConnectionStrings:DataAccessErMasterMySqlProvider");
             _radisCacheServerAddress = configuration.GetValue<string>("radishCache");
+            _ClientAPIUrl = configuration.GetValue<string>("ClientAPIURL");
         }
         public void OnException(ExceptionContext context)
         {
             var controllerName = (string)context.RouteData.Values["controller"];
             var actionName = (string)context.RouteData.Values["action"];
             string _token = Convert.ToString(context.HttpContext.Request.Headers["X-Authorized-Token"]);
+            string ClientAPIResponse = string.Empty;
+
             Authenticate authenticate = new Authenticate();
             authenticate = SecurityService.GetAuthenticateDataFromToken(_radisCacheServerAddress, SecurityService.DecryptStringAES(_token));
 
             var exceptionType = context.Exception.GetType();
             context.ExceptionHandled = true;
+
+            /*
             ErrorLogCaller errorLogCaller = new ErrorLogCaller();
             ErrorLog errorLogs = new ErrorLog
             {
@@ -57,6 +65,30 @@ namespace Easyrewardz_TicketSystem.WebAPI.Filters
                 IPAddress = Convert.ToString(context.HttpContext.Connection.RemoteIpAddress)
             };
             int result = errorLogCaller.AddErrorLog(new ErrorLogging(_ErconnectioSting), errorLogs);
+
+            */
+            try
+            {
+                ElasticErrorLogModel Elastic = new ElasticErrorLogModel
+                {
+                    applicationId = "2",
+                    actionName = actionName,
+                    controllerName = controllerName,
+                    tenantID = Convert.ToString(authenticate.TenantId),
+                    userID = Convert.ToString(authenticate.UserMasterID),
+                    exceptions = context.Exception.StackTrace,
+                    messageException = context.Exception.Message,
+                    ipAddress = Convert.ToString(context.HttpContext.Connection.RemoteIpAddress)
+                };
+
+                ClientAPIResponse = CommonService.SendApiRequest(_ClientAPIUrl + "api/InsertApplicationLog", JsonConvert.SerializeObject(Elastic));
+
+            }
+            catch(Exception)
+            {
+
+            }
+          
             context.Result = new RedirectToRouteResult(
              new RouteValueDictionary
              {
