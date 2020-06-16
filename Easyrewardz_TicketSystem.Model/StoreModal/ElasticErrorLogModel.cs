be4de4dbbcf69187d05feff4dc952aff3c155c6c
chetan.poojari <chetan.poojari@BVIMUMLAP-20>
2020-06-16 23:05:58 +0530

    commit:

---
+++ Easyrewardz_TicketSystem.Model/StoreModal/ElasticErrorLogModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easyrewardz_TicketSystem.Model.StoreModal
+{
+    public class ElasticErrorLogModel
+    {
+        public string applicationId { get; set; }
+        public string actionName { get; set; }
+        public string controllerName { get; set; }
+        public string tenantID { get; set; }
+        public string userID { get; set; }
+        public string exceptions { get; set; }
+        public string messageException { get; set; }
+        public string ipAddress { get; set; }
+    }
+}
