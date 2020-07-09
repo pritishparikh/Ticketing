7e40fc84cfef2648f42797fc8a2647e95fdf7326
chetan.poojari <chetan.poojari@BVIMUMLAP-20>
2020-07-09 19:34:29 +0530

    commit : api for getting store pay link

---
+++ Easyrewardz_TicketSystem.Interface/StoreInterface/IStorePay.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Easyrewardz_TicketSystem.CustomModel;
+
+namespace Easyrewardz_TicketSystem.Interface
+{
+    public interface IStorePay
+    {
+        string GenerateStorePayLink(int TenantID, string ProgramCode, int UserID,
+            string clientAPIUrlForGenerateToken, string clientAPIUrlForGeneratePaymentLink, HSRequestGenerateToken hSRequestGenerateToken);
+
+    }
+}
---
+++ Easyrewardz_TicketSystem.WebAPI/Provider/Store/StorePayCaller.cs
@@ -0,0 +1,40 @@
+﻿using Easyrewardz_TicketSystem.CustomModel;
+using Easyrewardz_TicketSystem.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Easyrewardz_TicketSystem.WebAPI.Provider.Store
+{
+    public class StorePayCaller
+    {
+        #region Variable declaration
+
+        private IStorePay _storePay;
+        #endregion
+
+
+
+        #region Methods 
+
+        /// <summary>
+        /// Generate Store Pay Link
+        /// </summary>
+        /// <param name="TenantID"></param>
+        /// <param name="ProgramCode"></param>
+        /// <param name="UserID"></param>
+        ///  <param name="clientAPIUrlForGenerateToken"></param>
+        ///   <param name="clientAPIUrlForGeneratePaymentLink"></param>
+        ///    <param name="hSRequestGenerateToken"></param>
+        /// <returns></returns>
+        public string GenerateStorePayLink(IStorePay storePay, int TenantID, string ProgramCode, int UserID,
+            string clientAPIUrlForGenerateToken, string clientAPIUrlForGeneratePaymentLink, HSRequestGenerateToken hSRequestGenerateToken)
+        {
+            _storePay = storePay;
+            return _storePay.GenerateStorePayLink(TenantID, ProgramCode, UserID, clientAPIUrlForGenerateToken, clientAPIUrlForGeneratePaymentLink, hSRequestGenerateToken);
+        }
+
+        #endregion
+    }
+}
